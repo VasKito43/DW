28f4a3cce69f47f0d7bbe4f6517beb284f6b52b1
VasKito43 <130059430+VasKito43@users.noreply.github.com>
2023-05-18 09:28:51 -0300

    18/05

--- python/matriz/matriz.py
+++ python/matriz/matriz.py
@@ -24,6 +24,10 @@ def verificacao(l,n):
         if soma != cm:
             print('!!falha!!')
 
+def mostrar(l):
+    for i in range(len(l)):
+        print(l[i])
+
 
 def s_t(n):
     s = ((n**2 + 1)/2) * (n**2)
@@ -50,15 +54,15 @@ def matriz_3():
         [0, 0, 0]
     ]
     sm = s_m(3)
-    l[0][0] = 2
-    l[1][2] = 3
-    l[2][1] = 1
-    l[1][1] = centro(3)
-    l[0][1] = c_m(3) - (l[1][1] + l[2][1])
-    l[0][2] = c_m(3) - (l[0][0] + l[0][1])
-    l[2][2] = c_m(3) - (l[0][2] + l[1][2])
-    l[2][0] = c_m(3) - (l[2][1] + l[2][2])
-    l[1][0] = c_m(3) - (l[2][0] + l[0][0])
+    l[0][0] = 2 #a11
+    l[1][2] = 3 #a23
+    l[2][1] = 1 #a32
+    l[1][1] = centro(3) #a22
+    l[0][1] = c_m(3) - (l[1][1] + l[2][1]) #a12
+    l[0][2] = c_m(3) - (l[0][0] + l[0][1]) #a13
+    l[2][2] = c_m(3) - (l[0][2] + l[1][2]) #a33
+    l[2][0] = c_m(3) - (l[2][1] + l[2][2]) #a31
+    l[1][0] = c_m(3) - (l[2][0] + l[0][0]) #a21
 
     verificacao(l, 3)
 
@@ -66,5 +70,36 @@ def matriz_3():
     print (l[1])
     print (l[2])
 
-matriz_1()
-matriz_3()
+def matriz(n):
+    l=[]
+
+    for i in range(n):
+        k=[] 
+        for j in range(n):
+            k.append(0)
+        l.append(k)
+
+    m = int((n+1)/2)
+    y = int(((n**2+1)/2) + ((n - 1)/2))
+    x = int((n**2+1)/2)
+    p = n - 1
+    p2 = m - 1
+    sm = s_m(n)
+
+    l[0][0] = 2 
+    l[p2][p] = 3 
+    l[p][p2] = 1
+    l[p2][p2] = x
+    l[0][1] = sm - y
+    l[0][p2] = int(n**2)
+    l[0][p-1] = y + 2
+    l[0][p] = x + 1 - m
+    l[1][1] = l[0][0] + n
+
+    mostrar(l)
+
+#matriz_1()
+#matriz_3()
+matriz(5)
+print()
+matriz(7)
