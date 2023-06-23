6ffb04653407aa4acf63ed01582a1cdc570d94c0
VasKito43 <130059430+VasKito43@users.noreply.github.com>
2023-06-23 00:32:06 -0300

    36

--- python/matriz/matriz2.py
+++ python/matriz/matriz2.py
@@ -1,7 +1,7 @@
 def criador(n):
     v = '  '
-    x = []
-    l=[]
+    x = [];
+    l=[];
     for i in range (n):
        x.append(v) 
 
@@ -10,19 +10,45 @@ def criador(n):
 
     return l
 
-def criador2(l):
-    m = []
-    for i in range(1, len(l)+1):
+def criador2(n):
+    m = [];
+    x = [];
+    for i in range(1, n**2+1):
         m.append(i)
-    print(m)
+        if i % n == 0:
+            x.append(m)
+            m = []
+    return x
     
-
-
-    print(l)
 
 def mostrar(l):
     for i in range(len(l)):
         print(l[i])
     print()
 
-criador2(criador(3))
+def removedor(v):
+    global l
+    for i in range(len(l)):
+        for j in range(len(l)):
+            if l[i][j] == v:
+                l[i][j] = 0
+
+n = int(input('n: '))
+m = criador(n)
+l = criador2(n)
+
+x = int((n+1)/2)
+y = n-1
+
+#valores fixos
+
+m[0][0] = 2
+m[x][x] = int(n**2/2)
+m[x][y] = 1
+m[y][x] = 3
+removedor(m[0][0])
+removedor(m[x][x])
+removedor(m[x][y])
+removedor(m[y][x])
+
+mostrar (m)
