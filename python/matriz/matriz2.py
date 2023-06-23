e0412c1832728a558c72f67baaa3c7c34d8681f1
VasKito43 <130059430+VasKito43@users.noreply.github.com>
2023-06-23 18:13:02 -0300

    m

--- python/matriz/matriz2.py
+++ python/matriz/matriz2.py
@@ -1,12 +1,11 @@
 def criador(n):
-    v = '  '
-    x = [];
-    l=[];
-    for i in range (n):
-       x.append(v) 
+    l=[]
 
-    for i in range (n):
-        l.append(x)
+    for i in range(n):
+        k=[] 
+        for j in range(n):
+            k.append(' ')
+        l.append(k)
 
     return l
 
@@ -33,22 +32,50 @@ def removedor(v):
             if l[i][j] == v:
                 l[i][j] = 0
 
-n = int(input('n: '))
+n = 7#int(input('n: '))
 m = criador(n)
 l = criador2(n)
 
-x = int((n+1)/2)
-y = n-1
+x = int((n+1)/2) - 1 #localização da metade da matriz
+y = n-1 # localização de 'n' na lista
+z = int((n**2+1)/2)
+sm = int(n**2+1) #S.M
+
 
 #valores fixos
 
 m[0][0] = 2
-m[x][x] = int(n**2/2)
-m[x][y] = 1
-m[y][x] = 3
+m[x][x] = z
+m[x][y] = 3
+m[y][x] = 1
+
+m[0][x] = sm - 1
+m[y][y] = sm - 2
+m[x][0] = sm - 3
+
 removedor(m[0][0])
 removedor(m[x][x])
 removedor(m[x][y])
 removedor(m[y][x])
+
+c = int((n+1)/2)
+p = n - 1
+for i in range(n): #primeira diagonal
+    c = c - 1
+    m[i][p-i] = z - c
+
+for i in range(x-1): #segunda diagonal
+    m[i+1][i+1] = m[i][i] + n
+
+for i in range(y, x+1, -1): #segunda diagonal
+    m[i-1][i-1] = m[i][i] - n
+
+for i in range(x-1): #coluna central
+    m[i+1][x] = m[i][x] - n
+
+for i in range(y, x+1, -1): #coluna central
+    m[i-1][x] = m[i][x] + n
+
+
 
 mostrar (m)
